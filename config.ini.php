bafaa97a993d1cdeafb584492ff7fef8fcbb998c
Daniel Santos <daniel.santos.ap@gmail.com>
2018-01-13 17:36:57 -0200

    alterações no projeto inicial

--- config.ini.php
+++ config.ini.php
@@ -11,10 +11,10 @@ framework = bootstrap
 
 [banco_de_dados]
 driver=mysql
-banco=kingkern_iceberg
-usuario=kingkern_iceberg
-senha=claudi741099@@
-host=138.128.175.66
+banco=campanha
+usuario=root
+senha=
+host=127.0.0.1
 
 [offlinedb]
 driver=mysql
