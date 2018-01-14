6b80d18e97af8cfb916b7727d6d445a5878deba8
Daniel Santos <daniel.santos.ap@gmail.com>
2018-01-14 17:22:50 -0200

    up

--- app/visao/index_visao/index_visao.php
+++ app/visao/index_visao/index_visao.php
@@ -1,5 +1,6 @@
 <?php
 $pagina = new page_site;
+$pagina->headersinclude .= '<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">';
 	$card = new cardshow;
 $pagina->bodycontent = $card->html();
 $pagina->render();
