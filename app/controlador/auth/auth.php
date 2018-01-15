1fb89be75d40e1646ca1736f5f1cb3424d4fa904
Daniel Santos <daniel.santos.ap@gmail.com>
2018-01-15 01:14:12 -0200

    autenticação

---
+++ app/controlador/auth/auth.php
@@ -0,0 +1,38 @@
+<?php
+class auth {
+	private $user;
+	private $password;
+
+	public function __construct(){
+	}
+	public function index(){
+		/**
+		captura os dados do formulario
+		**/
+		$this->user = $_POST["user"];
+		$this->password = sha1(md5(sha1($_POST["snhpwd"])));
+		//cria a query
+		$sql = "call sp_login(\"$this->user\", \"$this->password\")";
+		$dados = retornadbinfo($sql);
+		$linha = $dados->fetch(PDO::FETCH_ASSOC);
+		if ($linha["existe"] == 1){
+			$_SESSION["LOGADO"]=TRUE;
+			$_SESSION["usuario"] = $_POST["user"];
+			//$_SESSION["usuario"]->get_dadosuser();
+			header("Location: /");
+			echo "<script>document.reload();</script>";
+		} else {
+			echo "login desativado ou inexistente : ";
+			echo $this->password;
+		};
+	}
+	public function logout(){
+		session_unset($_SESSION["LOGADO"]);
+		session_destroy();
+		header("Location: /");
+		echo "<script>document.reload();</script>";
+		echo "saiu";
+	}
+	
+}
+?>
