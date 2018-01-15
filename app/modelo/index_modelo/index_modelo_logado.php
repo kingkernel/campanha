880220adda6fdecd8f60bac67421f91ad6584a92
Daniel Santos <daniel.santos.ap@gmail.com>
2018-01-15 04:15:16 -0200

    up

--- app/modelo/index_modelo/index_modelo_logado.php
+++ app/modelo/index_modelo/index_modelo_logado.php
@@ -4,6 +4,13 @@ $pagina = new page_site;
 	$card->imgavatar =  PUBLICDIR ."images/avatar.jpg";
 	$card->imgfundo = PUBLICDIR . "images/portico.jpg";
 	$nav = new topnav;
+		$admin_menu = new li_item;
+		$admin_menu->text = "menu";
+
+		$use_info = new li_user_info;
+		$use_info->nomedisplay = "usuario";
+	$nav->itensleft = [$admin_menu];
+	$nav->itensright = [$use_info];
 $pagina->headersinclude .="<style>".$card->addcss."</style>";
 $pagina->scriptsendpage = $card->addjsend;
 $pagina->bodycontent = $nav->html() . $card->html();
