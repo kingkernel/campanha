1ce94115593cb3a1e479603c54a020c199d939ea
Daniel Santos <daniel.santos.ap@gmail.com>
2018-01-18 01:58:05 -0200

    up

--- app/controlador/index/index.php
+++ app/controlador/index/index.php
@@ -7,6 +7,7 @@ class index {
 		if (!isset($_SESSION["LOGADO"])){
 		include_once(PATHMODELO . get_class() ."_modelo/". get_class()."_modelo.php");
 		} else {
+			print_r($_SESSION);
 			include_once(PATHMODELO . get_class() ."_modelo/". get_class()."_modelo_logado.php");
 		};
 	}
