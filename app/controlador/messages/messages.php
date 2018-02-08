0c342b42771b338eb1f2ad319a8f9a08789a1716
Daniel Santos <daniel.santos.ap@gmail.com>
2018-02-08 01:57:48 -0200

    up

--- app/controlador/messages/messages.php
+++ app/controlador/messages/messages.php
@@ -18,7 +18,7 @@ class  messages {
 		$menuup = new topmenu_campanha;
 
 		$pagina->scriptsendpage ='CKEDITOR.replace(\'editor1\');';
-		$pagina->bodycontent = $menuup->html().'<div style="margin:20px"><textarea name="editor1" id="editor1"></textarea><script type="text/javascript" src="'.PUBLICDIR.'3ptn/ckeditor/ckeditor.js"></script></div>'."<br/><br/><br/><br/><br/>";
+		$pagina->bodycontent = $menuup->html().'<div style="margin:20px"><textarea name="editor1" id="editor1"></textarea><script type="text/javascript" src="'.PUBLICDIR.'3ptn/ckeditor/ckeditor.js"></script></div><div name="controls" style="margin:20px"><button class="btn btn-primary">Enviar</button></div>'."<br/><br/><br/><br/><br/>";
 		$pagina->render();
 		
 	}
