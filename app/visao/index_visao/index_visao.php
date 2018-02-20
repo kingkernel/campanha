a648da69b00d8f13116b180de12939c31c22d722
Daniel Santos <daniel.santos.ap@gmail.com>
2018-02-19 22:06:05 -0300

    up

--- app/visao/index_visao/index_visao.php
+++ app/visao/index_visao/index_visao.php
@@ -3,6 +3,6 @@ $pagina = new page_site;
 	$card = new cardshow;
 	$card->imagemlogin = PUBLICDIR ."images/avatar.jpg";
 	$carousel = new carousel;
-$pagina->bodycontent = $card->html().$carousel->html();
+$pagina->bodycontent = $carousel->html().$card->html().;
 $pagina->render();
 ?>
